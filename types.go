5832be15450905c7ae3c9dc827cd25288daea8db
Abdeali Jaroli <abdealiburu03@gmail.com>
2024-08-11 19:56:08 +0530

    feat: implement authentication context for request handling

--- types.go
+++ types.go
@@ -1,34 +1,61 @@
 package main
 
 import (
+	"errors"
 	"math/rand"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
+type LoginRequest struct {
+	Email             string `json:"email"`
+	EncryptedPassword string `json:"password"`
+}
+
 type TransferRequest struct {
-	ToAccount int `json:"toAccount"`
-	Amount    int `json:"amount"`
+	FromAccount int   `json:"fromAccount"`
+	ToAccount   int   `json:"toAccount"`
+	Amount      int64 `json:"amount"`
 }
 
 type Account struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Phone     int64     `json:"phone"`
-	Balance   int64     `json:"balance"`
-	CreatedAt time.Time `json:"createdAt"`
+	ID                int       `json:"id"`
+	FirstName         string    `json:"firstName"`
+	LastName          string    `json:"lastName"`
+	Email             string    `json:"email"`
+	Phone             int64     `json:"phone"`
+	EncryptedPassword string    `json:"-"`
+	Balance           int64     `json:"balance"`
+	CreatedAt         time.Time `json:"createdAt"`
 }
 
 type NewAccount struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
+	FirstName         string `json:"firstName"`
+	LastName          string `json:"lastName"`
+	Email             string `json:"email"`
+	EncryptedPassword string `json:"-"`
 }
 
-func GenerateNewAccount(firstname, lastname string) *Account {
-	return &Account{
-		FirstName: firstname,
-		LastName:  lastname,
-		Phone:     int64(rand.Intn(1e5)),
-		CreatedAt: time.Now().UTC(),
+func GenerateNewAccount(firstname, lastname, email, password string) (*Account, error) {
+	if email == "" {
+		return nil, errors.New("email cannot be empty")
 	}
+	if password == "" {
+		return nil, errors.New("password cannot be empty")
+	}
+
+	enpw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Account{
+		FirstName:         firstname,
+		LastName:          lastname,
+		Email:             email,
+		EncryptedPassword: string(enpw),
+		Phone:             int64(rand.Intn(1e5)),
+		CreatedAt:         time.Now().UTC(),
+	}, nil
 }
